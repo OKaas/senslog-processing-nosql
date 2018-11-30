7f5ea065b017cf019166f608c1de943391db8055
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-11-30 16:02:01 +0100

    Automatic fill of database with test data

--- src/main/java/cz/senslog/processing/ApplicationConfiguration.java
+++ src/main/java/cz/senslog/processing/ApplicationConfiguration.java
@@ -1,13 +1,32 @@
 package cz.senslog.processing;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 /**
  * Created by OK on 02-Nov-18.
  */
 @Configuration
 public class ApplicationConfiguration {
-    // TODO just for sure :)
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[] { new ClassPathResource("data.json")});
+        return factory;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper;
+    }
 }
 
 
